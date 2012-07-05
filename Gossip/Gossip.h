699b12d006cb965760ba987fa15e7b1c90ffffb1
Chakrit Wichian <chakrit@oozou.com>
2012-07-05 16:40:51 +0700

    Initial project creation.

---
+++ Gossip/Gossip.h
@@ -0,0 +1,13 @@
+//
+//  Gossip.h
+//  Gossip
+//
+//  Created by Chakrit Wichian on 7/5/12.
+//  Copyright (c) 2012 Oozou Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Gossip : NSObject
+
+@end
---
+++ Gossip/Gossip.m
@@ -0,0 +1,13 @@
+//
+//  Gossip.m
+//  Gossip
+//
+//  Created by Chakrit Wichian on 7/5/12.
+//  Copyright (c) 2012 Oozou Ltd. All rights reserved.
+//
+
+#import "Gossip.h"
+
+@implementation Gossip
+
+@end
