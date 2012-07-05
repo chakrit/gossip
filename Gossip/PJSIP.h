74c4da450fd746a8f230ba75d739005d3d6e3972
Chakrit Wichian <chakrit@oozou.com>
2012-07-05 17:33:18 +0700

    Add pjsip include paths + test it out.

---
+++ Gossip/PJSIP.h
@@ -0,0 +1,22 @@
+//
+//  PJSIP.h
+//  Gossip
+//
+//  Created by Chakrit Wichian on 7/5/12.
+//
+
+/// Shim for loading PJSIP stuff with all the required #defines
+
+// NOTE: Must be placed *before* any pjsip stuff.
+#ifndef PJ_IS_LITTLE_ENDIAN
+#define PJ_IS_LITTLE_ENDIAN 1
+#endif
+
+#ifndef PJ_IS_BIG_ENDIAN
+#define PJ_IS_BIG_ENDIAN 0
+#endif
+
+#import <pj/config_site.h>
+
+// Place any requried PJSIP includes/imports *below* this line
+#import <pjsua-lib/pjsua.h>
