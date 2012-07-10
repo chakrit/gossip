dfa62779c74300e6f1d082c97c0c8805efa0c140
Chakrit Wichian <chakrit@oozou.com>
2012-07-10 18:13:24 +0700

    Prevent KVO notices when the value doesn't really changes.

--- Gossip/GSAccount.m
+++ Gossip/GSAccount.m
@@ -116,6 +116,9 @@
 
 
 - (void)setStatus:(GSAccountStatus)newStatus {
+    if (_status == newStatus) // don't send KVO notices unless it really changes.
+        return;
+    
     [self willChangeValueForKey:@"status"];
     _status = newStatus;
     [self didChangeValueForKey:@"status"];
