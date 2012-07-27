25c0d203596706a82b864520902c6f735b72e64c
Chakrit Wichian <chakrit@oozou.com>
2012-07-27 14:55:28 +0700

    Documentation update.

--- search/all_61.js
+++ search/all_61.js
@@ -4,6 +4,7 @@ var searchData=
   ['account_3adidreceiveincomingcall_3a',['account:didReceiveIncomingCall:',['../protocol_g_s_account_delegate-p.html#af5d41e244d0c3d413ebb978753b4bb8b',1,'GSAccountDelegate-p']]],
   ['accountid',['accountId',['../interface_g_s_account.html#a43d57805c8ac4d8546cd7ba42c74c3f1',1,'GSAccount']]],
   ['address',['address',['../interface_g_s_account_configuration.html#a444397aa067cf65287a72615bdbdf076',1,'GSAccountConfiguration']]],
+  ['allowstatuspublishing',['allowStatusPublishing',['../interface_g_s_account_configuration.html#a3c8510f03795c5b323e36c0ca7863863',1,'GSAccountConfiguration']]],
   ['arrayofavailablecodecs',['arrayOfAvailableCodecs',['../interface_g_s_user_agent.html#a88aec5696df351dd7b6364eacc6c296e',1,'GSUserAgent']]],
   ['authrealm',['authRealm',['../interface_g_s_account_configuration.html#af77dd285e217d1dc73aa63c46ff1ac3c',1,'GSAccountConfiguration']]],
   ['authscheme',['authScheme',['../interface_g_s_account_configuration.html#ac9ed3de07a10f04e839bf9f880a1f26a',1,'GSAccountConfiguration']]]
